7d90ae13dfd3eb962409cbcf2a662d7ee41f41c9
Akachukwu <egbolucheakachukwu@gmail.com>
2024-02-21 16:05:12 +0100

    Changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,57 +4,59 @@ import { motion } from 'framer-motion';
 import { transition1 } from '../transitions';
 import { useContext } from 'react';
 import { CursorContext } from '../context/CursorContext';
+import Newsletter from './NewsLetter';
 
 const Home = () => {
   const { mouseEnterHandler, mouseLeaverHandler } = useContext(CursorContext)
   return (
-      <motion.section
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        transition={transition1}
-        className="section"
-      >
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={transition1}
+      className="section overflow-auto"
+    >
 
-        <div className="container mx-auto h-full relative">
-          <div className='flex flex-col justify-center'>
+      <div className="container mx-auto h-full relative">
+        <div className='flex flex-col justify-center'>
+          <motion.div
+            initial={{ opacity: 0, y: '-50%' }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: '-50%' }}
+            transition={transition1}
+            onMouseEnter={mouseEnterHandler}
+            onMouseLeave={mouseLeaverHandler}
+            className='w-full pt-36 pb-14 lg:pt-0 lg:pb-0 lg:w-auto z-10 lg:absolute flex flex-col justify-center items-center lg:items-start'>
+            <h1 className="h1 text-center">
+              Author <br /> & Model
+            </h1>
+            <p className='text-[26px] lg:text-[36px] font-primary mb-4 lg:mb-12'>
+              Manhattan, New York, USA
+            </p>
+            <Link to={'/about'} className='btn mb-[30px]'>
+              View   Bio
+            </Link>
+          </motion.div>
+          <div className='flex justify-end max-h-96 lg:max-h-max'>
             <motion.div
-              initial={{ opacity: 0, y: '-50%' }}
-              animate={{ opacity: 1, y: 0 }}
-              exit={{ opacity: 0, y: '-50%' }}
+              initial={{ scale: 0 }}
+              animate={{ scale: 1 }}
+              exit={{ scale: 0 }}
               transition={transition1}
-              onMouseEnter={mouseEnterHandler}
-              onMouseLeave={mouseLeaverHandler}
-              className='w-full pt-36 pb-14 lg:pt-0 lg:pb-0 lg:w-auto z-10 lg:absolute flex flex-col justify-center items-center lg:items-start'>
-              <h1 className="h1 text-center">
-                Author <br /> & Model
-              </h1>
-              <p className='text-[26px] lg:text-[36px] font-primary mb-4 lg:mb-12'>
-                Manhattan, New York, USA
-              </p>
-              <Link to={'/about'} className='btn mb-[30px]'>
-                View   Bio
-              </Link>
-            </motion.div>
-            <div className='flex justify-end max-h-96 lg:max-h-max'>
-              <motion.div
-                initial={{ scale: 0 }}
-                animate={{ scale: 1 }}
-                exit={{ scale: 0 }}
+              className='relative lg:-right-40 overflow-hidden'>
+              <motion.img
+                whileHover={{ scale: 1.1 }}
                 transition={transition1}
-                className='relative lg:-right-40 overflow-hidden'>
-                <motion.img
-                  whileHover={{ scale: 1.1 }}
-                  transition={transition1}
-                  src={DenceImg} alt="Confidence Ezemba"
-                  width={761}
-                  height={1037}
-                />
-              </motion.div>
-            </div>
+                src={DenceImg} alt="Confidence Ezemba"
+                width={761}
+                height={1037}
+              />
+            </motion.div>
           </div>
         </div>
-      </motion.section>
+      </div>
+      <Newsletter />
+    </motion.section>
   )
 }
 
